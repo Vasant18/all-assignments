60cef3e601822b159be72871a6a750b947a43941
Vasant18 <vasantspujar018@gmail.com>
2024-08-25 00:46:50 +0530

    executed and modified script-ugly.js file

--- week-3/01-reconciler/script-ugly.js
+++ week-3/01-reconciler/script-ugly.js
@@ -31,9 +31,14 @@ function createDomElements(data) {
     console.log(added);
   }
   
+  
+  //THE ABOVE IS i.e createDomElements THE RECOSILER FUNCTION , we have done the updating and deleting logic in this function so that others don't need to dwell into the DOM and all things  they just specify things like what to be added and deleted and how many times etc ,for like this in numerous  other cases , THIS IS WHAT REACT DOES BEHIND THE SCENES.
+
+  
+  
   window.setInterval(() => {
     let todos = [];
-    for (let i = 0; i<Math.floor(Math.random() * 100); i++) {
+    for (let i = 0; i < Math.floor(Math.random() * 100); i++) {
       todos.push({
         title: "Go to gym",
         description: "Go to gym form 5",
@@ -44,3 +49,79 @@ function createDomElements(data) {
     createDomElements(todos)
   }, 5000)
   
+
+
+
+  /**  USING REACT TO IMPLEMT THE ABOVE FUNCTIONALITY 
+
+
+  *Set Up a React Project:
+       npx create-react-app my-react-app
+       cd my-react-app
+
+
+*Create the React Component:
+ import React, { useState, useEffect } from 'react';
+
+function TodoList() {
+  const [todos, setTodos] = useState([]);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      const newTodos = [];
+      for (let i = 0; i < Math.floor(Math.random() * 100); i++) {
+        newTodos.push({
+          title: "Go to gym",
+          description: "Go to gym from 5",
+          id: i + 1
+        });
+      }
+      setTodos(newTodos);
+    }, 5000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  const deleteTodo = (id) => {
+    setTodos(todos.filter(todo => todo.id !== id));
+  };
+
+  return (
+    <div id="mainArea">
+      {todos.map(item => (
+        <div key={item.id} data-id={item.id}>
+          <span>{item.title}</span>
+          <span>{item.description}</span>
+          <button onClick={() => deleteTodo(item.id)}>Delete</button>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default TodoList;
+
+
+
+
+*Render the React Component:
+
+Use the Component in Your App:
+import React from 'react';
+import './App.css';
+import TodoList from './TodoList';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Todo List</h1>
+        <TodoList />
+      </header>
+    </div>
+  );
+}
+
+export default App;
+
+   */
